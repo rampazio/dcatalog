3a559809ec8c0b47583a76124138b3b4611744a3
rampazio <diegorfreitas3009@gmail.com>
2022-01-20 16:04:08 -0300

    backend project created

---
+++ backend/src/main/java/com/rampazio/Dcatalog/DcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rampazio.Dcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rampazio/Dcatalog/DcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rampazio.Dcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
