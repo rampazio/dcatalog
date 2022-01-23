5bdea1089f8fb33d6a9e223d522dae57ce419e4f
rampazio <diegorfreitas3009@gmail.com>
2022-01-23 19:02:42 -0300

    Seed BD

--- backend/src/main/java/com/rampazio/Dcatalog/services/CategoryService.java
+++ backend/src/main/java/com/rampazio/Dcatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rampazio.Dcatalog.entities.Category;
 import com.rampazio.Dcatalog.repositories.CategoryRepository;
@@ -15,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 		
